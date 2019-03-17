211ae9afe3dbf69389c1724ffccd08a354262d78
CodeatEase <technology3.0cradle@gmail.com>
2019-03-17 14:00:22 +0530

    Update Tag_Content_Display.py

--- Tag_Content_Display.py
+++ Tag_Content_Display.py
@@ -4,7 +4,7 @@ file_list = glob.glob(r'C:\Users\Nachiketh Atawale\Downloads\*.dcm')
 for files in file_list:
     print ("DICOM File", files) # For indicating the file being processed. 
     Patient = pydicom.dcmread(files)
-    with open(r"C:\Users\Nachiketh Atawale\Downloads\Output.txt", 'a') as output: 
+    with open(r"C:\Users\"User_name"\Downloads\Output.txt", 'a') as output: 
         print ("DICOM File : ",files,"\n\n\t","Tags present in the file : \n\n\t\t",Patient.dir(),"\n\n\t","Patient Information :","\n\n", Patient,"\n\n\n", file = output)
         print ("\n")
 output.close()      
